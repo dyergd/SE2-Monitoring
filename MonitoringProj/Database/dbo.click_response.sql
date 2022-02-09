46b1bea40dc4cce3ad71443d8bbec11ffead95a0
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2022-02-08 22:56:13 -0500

    Adding tables and testing script to new project
    After Garrett redid the program, Fixed the issue of missing tables and scripts.

---
+++ MonitoringProj/Database/dbo.click_response.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE  [dbo].[click_response]
+(
+	click_id int,
+	tstamp datetime2,
+	response_code int,
+	message varchar(30),
+	CONSTRAINT click_response_PK PRIMARY KEY (click_id, tstamp)
+)
---
+++ MonitoringProj/Database/dbo.visit_clicks.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE  [dbo].[visit_clicks]
+(
+	click_id int IDENTITY(1,1),
+	visit_id int,
+	click_tstamp datetime2,
+	currentpage varchar,
+	nextpage varchar,
+	x int,
+	y int,
+	CONSTRAINT visit_clicks_click_id_PK PRIMARY KEY (click_id),
+	CONSTRAINT visit_clicks_visit_id_FK FOREIGN KEY (visit_id)
+	REFERENCES VISIT(visit_id)
+)
